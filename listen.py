605b631d69f5e604ed316e00930a54e9fddd9b82
Graham Williams <Graham.Williams@togaware.com>
2020-06-27 06:42:23 +1000

    Update to new mlhub.

--- listen.py
+++ listen.py
@@ -1,74 +1,47 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# -*- coding: utf-8 -*-
+#
+# Time-stamp: <Saturday 2020-06-27 06:40:14 AEST Graham Williams>
+#
+# Copyright (c) Togaware Pty Ltd. All rights reserved.
 # Licensed under the MIT License.
 # Author: Graham.Williams@togaware.com
 #
-# This demo is based on the Azure Cognitive Services Speech to Text Quick Start
+# Command line tool to capture and transcribe speech.
 
-# Defaults.
-
-KEY_FILE = "private.py"
-DEFAULT_REGION = "southeastasia"
-
-subscription_key = None
-region = DEFAULT_REGION
+# ----------------------------------------------------------------------
+# Setup
+# ----------------------------------------------------------------------
 
 # Import the required libraries.
 
 import os
 import sys
+
 import azure.cognitiveservices.speech as speechsdk
 
-#from textwrap import fill
+from mlhub.pkg import azkey
 
-# Prompt the user for the key and region and save into private.py for
-# future runs of the model. The contents of that file is:
-#
-# subscription_key = "a14d...ef24"
-# region = "southeastasia"
+# ----------------------------------------------------------------------
+# Request subscription key and location from user.
+# ----------------------------------------------------------------------
 
-if os.path.isfile(KEY_FILE) and os.path.getsize(KEY_FILE) != 0:
-    exec(open(KEY_FILE).read())
-else:
-    sys.stdout.write("Please enter your Speech Services subscription key []: ")
-    subscription_key = input()
+SERVICE   = "Speech"
+KEY_FILE  = os.path.join(os.getcwd(), "private.txt")
 
-    sys.stdout.write("Please enter your region [southeastasia]: ")
-    region = input()
-    if len(region) == 0: region = DEFAULT_REGION
+key, location = azkey(KEY_FILE, SERVICE, connect="location", verbose=False)
 
-    if len(subscription_key) > 0:
-        assert subscription_key
-        ofname = open(KEY_FILE, "w")
-        ofname.write("""subscription_key = "{}"
-region = "{}"
-    """.format(subscription_key, region))
-        ofname.close()
+#-----------------------------------------------------------------------
+# Set up a speech recognizer.
+#-----------------------------------------------------------------------
 
-        print("""
-I've saved that information into the file:
-
-        """ + os.getcwd() + "/" + KEY_FILE)
-
-########################################################################
-#
-# Following is the code that does the actual work, creating an
-# instance of a speech config with the specified subscription key and
-# service region, then creating a recognizer with the given settings,
-# and then performing recognition. recognize_once() returns when the
-# first utterance has been recognized, so it is suitable only for
-# single shot recognition (up to 15 seconds) like command or
-# query. For long-running recognition, use
-# start_continuous_recognition() instead, or if you want to run
-# recognition in a non-blocking manner, use recognize_once_async().
-
-speech_config     = speechsdk.SpeechConfig(subscription=subscription_key, region=region)
+speech_config     = speechsdk.SpeechConfig(subscription=key, region=location)
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
-result            = speech_recognizer.recognize_once()
 
-#
-########################################################################
+#-----------------------------------------------------------------------
+# Trasnscribne some spoken words.
+#-----------------------------------------------------------------------
 
-# Checks result.
+result = speech_recognizer.recognize_once()
 
 if result.reason == speechsdk.ResultReason.RecognizedSpeech:
     print("{}".format(result.text))
