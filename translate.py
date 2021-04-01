785e4367c3405926642740519924605cc061a3d8
jingjinggit config --global user.email 745287546@qq.com <745287546@qq.com>
2021-04-01 12:23:59 +1100

    Add existing language codes into dictionary.

--- translate.py
+++ translate.py
@@ -8,6 +8,7 @@ import os
 import azure.cognitiveservices.speech as speechsdk
 import argparse
 from mlhub.pkg import azkey
+import pandas
 
 # -----------------------------------------------------------------------
 # Process the command line
@@ -47,6 +48,26 @@ KEY_FILE = os.path.join(os.getcwd(), "private.txt")
 speech_key, service_region = azkey(KEY_FILE, SERVICE, connect="location", verbose=False)
 
 # ----------------------------------------------------------------------
+# Get language code. "Original": is from Loale in Speech-to-text table.
+# "target": is from Language Code in Text languages table.
+# Make sure the text language in Text Language table exists in Neural voices
+# table.
+# ----------------------------------------------------------------------
+language_to_voice_map = {}
+dataframe_speech = pandas.read_csv("text-to-speech.txt",delimiter="\t")
+
+for index, row in dataframe_speech.iterrows():
+    if row[1] == "zh-HK":
+        language_code = "yue"
+    elif row[1] == "zh-CN":
+        language_code = "zh-Hant"
+
+    else:
+        language_code = row[1][0:2]
+
+    language_to_voice_map[language_code] = row[3]
+
+# ----------------------------------------------------------------------
 # Translate the speech to another language speech
 # ----------------------------------------------------------------------
 
@@ -70,14 +91,6 @@ def translate_speech_to_text():
 
 
 def synthesize_translations(result):
-    language_to_voice_map = {
-        "de": "de-DE-KatjaNeural",
-        "en": "en-US-AriaNeural",
-        "it": "it-IT-ElsaNeural",
-        "pt": "pt-BR-FranciscaNeural",
-        "zh-Hans": "zh-CN-XiaoxiaoNeural",
-        "nl": "nl-NL-ColetteNeural"
-    }
     print(f'Recognized: "{result.text}"')
     translation = result.translations[to_language]
     print(f'Translated into "{to_language}": {translation}')
