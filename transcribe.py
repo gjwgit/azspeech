28b94112793d8f134adae7daa59618e3f2e21d45
Graham Williams <Graham.Williams@togaware.com>
2022-03-13 21:37:36 +1100

    Upgrade style to using click.

--- transcribe.py
+++ transcribe.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 #
-# Time-stamp: <Monday 2021-06-07 14:28:59 AEST Graham Williams>
+# MLHub toolket for Azure Speech - Transcribe
 #
-# Copyright (c) Togaware Pty Ltd. All rights reserved.
-# Licensed under GPLv3.
+# Time-stamp: <Sunday 2022-03-13 21:26:59 +1100 Graham Williams>
+#
 # Author: Graham.Williams@togaware.com
+# Licensed under GPLv3.
+# Copyright (c) Togaware Pty Ltd. All rights reserved.
 #
 # ml transcribe azspeech
 
@@ -14,143 +16,159 @@
 
 # Import the required libraries.
 
-from mlhub.pkg import get_cmd_cwd, get_private
-import argparse
-import azure.cognitiveservices.speech as speechsdk
 import os
 import sys
-import textwrap
 import time
 import wave
+import click
+import textwrap
+
+import azure.cognitiveservices.speech as speechsdk
+
+from azure.cognitiveservices.speech import SpeechRecognizer
+from azure.cognitiveservices.speech.audio import AudioConfig
+
+from mlhub.pkg import get_cmd_cwd, get_private
+
+done = False
+
 
 # -----------------------------------------------------------------------
-# Process the command line
+# Command line argument and options
 # -----------------------------------------------------------------------
 
-option_parser = argparse.ArgumentParser(add_help=False)
+@click.command()
+@click.argument("filename",
+                default=None,
+                required=False,
+                type=click.STRING)
+@click.option("-l", "--lang",
+              default=None,
+              type=click.STRING,
+              help="The language of the source audio.")
+def cli(filename, lang):
+    """Transcribe an audio WAV file or audio from the microphone.
 
-# Currently only wav supported here. For mp3 some extra configuration
-# required.
+Currently only WAV audio is supported for the FILENAME. If no FILENAME
+is suppled then the audio is captured from the microphone.
 
-option_parser.add_argument(
-    '--input', "-i",
-    help='wav input file')
+The audio is sent off to the Auzue Cognitive Services to perform an
+AI based transcription. The result is returned as text.
 
-option_parser.add_argument(
-    '--lang', "-l",
-    help='source language')
+    """
+    # ----------------------------------------------------------------------
+    # Request subscription key and location from user.
+    # ----------------------------------------------------------------------
 
-args = option_parser.parse_args()
+    key, location = get_private()
 
-# ----------------------------------------------------------------------
-# Request subscription key and location from user.
-# ----------------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    # Set up a speech configuration.
+    # -----------------------------------------------------------------------
 
-key, location = get_private()
+    speech_config = speechsdk.SpeechConfig(subscription=key, region=location)
 
-# -----------------------------------------------------------------------
-# Set up a speech configuration.
-# -----------------------------------------------------------------------
+    if lang:
+        speech_config.speech_recognition_language = lang
 
-speech_config = speechsdk.SpeechConfig(subscription=key, region=location)
+    # -----------------------------------------------------------------------
+    # Transcribe file or from microphone.
+    # -----------------------------------------------------------------------
 
-if args.lang:
-    speech_config.speech_recognition_language = args.lang
+    if filename:
+        path = os.path.join(get_cmd_cwd(), filename)
+        if not os.path.exists(path):
+            sys.exit(f"azspeech transcribe: File not found: {path}")
 
-# -----------------------------------------------------------------------
-# Transcribe file or from microphone.
-# -----------------------------------------------------------------------
+        # Test that we have a suitable WAV file.
 
-if args.input:
-    path = os.path.join(get_cmd_cwd(), args.input)
-    if not os.path.exists(path):
-        sys.exit(f"azspeech transcribe: File not found: {path}")
+        try:
+            wave.open(path)
+        except Exception:
+            sys.exit(f"azspeech transcribe: File is not wav audio: {path}")
 
-    try:
-        w = wave.open(path)
-    except Exception as e:
-        # print(e)
-        sys.exit(f"azspeech transcribe: File does not seem to be wav audio: {path}")
+        # Create a callback to terminate the transcription once the full
+        # audio has been transcribed.
 
-    # Create a callback to terminate the transcription once the full
-    # audio has been transcribed.
+        def stop_cb(evt):
+            """Callback stop continuous recognition on receiving event (evt)"""
+            speech_recognizer.stop_continuous_recognition()
+            global done
+            done = True
 
-    done = False
+        # Create an audio configuration to load the audio from file rather
+        # than from microphone. A sample audio file is available as
+        # harvard.wav from:
+        #
+        # https://github.com/realpython/python-speech-recognition/raw/master/
+        # audio_files/harvard.wav
+        #
+        # A recognizer is then created with the given settings.
 
-    def stop_cb(evt):
-        """Callback to stop continuous recognition on receiving event (evt)"""
-        speech_recognizer.stop_continuous_recognition()
-        global done
-        done = True
+        audio_config = AudioConfig(use_default_microphone=False,
+                                   filename=path)
 
-    # Create an audio configuration to load the audio from file rather
-    # than from microphone. A sample audio file is available as
-    # harvard.wav from:
-    #
-    # https://github.com/realpython/python-speech-recognition/raw/master/
-    # audio_files/harvard.wav
-    #
-    # A recognizer is then created with the given settings.
+        speech_recognizer = SpeechRecognizer(speech_config=speech_config,
+                                             audio_config=audio_config)
 
-    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=False,
-                                               filename=path)
+        # Connect callbacks to the events fired by the speech
+        # recognizer. Most are commented out as examples here to allow
+        # tracing if you are interested in exploring the interactions with
+        # the server.
+        #
+        # speech_recognizer.recognizing.connect(lambda evt:
+        #                                 print('RECOGNIZING: {}'.format(evt)))
+        # speech_recognizer.session_started.connect(lambda evt:
+        #                                 print('STARTED: {}'.format(evt)))
+        # speech_recognizer.session_stopped.connect(lambda evt:
+        #                                 print('STOPPED {}'.format(evt)))
+        # speech_recognizer.canceled.connect(lambda evt:
+        #                                  print('CANCELED {}'.format(evt)))
 
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config,
-                                                   audio_config=audio_config)
+        # This callback provides the actual transcription.
 
-    # Connect callbacks to the events fired by the speech
-    # recognizer. Most are commented out as examples here to allow
-    # tracing if you are interested in exploring the interactions with
-    # the server.
-    #
-    # speech_recognizer.recognizing.connect(lambda evt:
-    #                                       print('RECOGNIZING: {}'.format(evt)))
-    # speech_recognizer.session_started.connect(lambda evt:
-    #                                           print('STARTED: {}'.format(evt)))
-    # speech_recognizer.session_stopped.connect(lambda evt:
-    #                                           print('STOPPED {}'.format(evt)))
-    # speech_recognizer.canceled.connect(lambda evt:
-    #                                    print('CANCELED {}'.format(evt)))
-    
-    # This callback provides the actual transcription.
+        def respond(evt):
+            print(f'{textwrap.fill(evt.result.text)}\n')
 
-    speech_recognizer.recognized.connect(lambda evt:
-                                         print(f'{textwrap.fill(evt.result.text)}\n'))
+        speech_recognizer.recognized.connect(respond)
 
-    # Stop continuous recognition on either session stopped or canceled
-    # events.
+        # Stop continuous recognition on either session stopped or canceled
+        # events.
 
-    speech_recognizer.session_stopped.connect(stop_cb)
-    speech_recognizer.canceled.connect(stop_cb)
+        speech_recognizer.session_stopped.connect(stop_cb)
+        speech_recognizer.canceled.connect(stop_cb)
 
-    # Start continuous speech recognition, and then perform
-    # recognition. For long-running recognition we use
-    # start_continuous_recognition().
+        # Start continuous speech recognition, and then perform
+        # recognition. For long-running recognition we use
+        # start_continuous_recognition().
 
-    speech_recognizer.start_continuous_recognition()
-    while not done: time.sleep(.5)
-    
-else:
+        speech_recognizer.start_continuous_recognition()
+        while not done:
+            time.sleep(.5)
 
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+    else:
 
-    #-----------------------------------------------------------------------
-    # Trasnscribe spoken words from the system microphone.
-    #-----------------------------------------------------------------------
+        speech_recognizer = SpeechRecognizer(speech_config=speech_config)
 
-    result = speech_recognizer.recognize_once()
+        # -----------------------------------------------------------------------
+        # Trasnscribe spoken words from the system microphone.
+        # -----------------------------------------------------------------------
 
-    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("{}".format(result.text))
-    elif result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(result.no_match_details))
-    elif result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("To update your key, please run ml configure azspeech.", file=sys.stderr)
-            sys.exit(1)
+        result = speech_recognizer.recognize_once()
+
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            print("{}".format(result.text))
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            print(f"No speech could be recognized: {result.no_match_details}")
+        elif result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = result.cancellation_details
+            print(f"Recognition canceled: {cancellation_details.reason}")
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print("Error details: f{cancellation_details.error_details}")
+                print("Update your key with 'ml configure azspeech'.",
+                      file=sys.stderr)
+                sys.exit(1)
 
 
-
+if __name__ == "__main__":
+    cli(prog_name="transcribe")
